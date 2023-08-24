d81f011089be632671a919471e3db8a057fd07f0
shubham07pr <138894848+shubham07pr@users.noreply.github.com>
2023-08-24 21:58:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+class Main {
+    public static void main(String[] args) {
+        JFrame frame = new Calculator();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
